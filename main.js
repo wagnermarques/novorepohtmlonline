f72780eb9b8ad24cf09d3fd211c0a18f957b3e2c
wagnermarques <wagnerdocri@gmail.com>
2026-04-29 20:39:44 -0300

    registrei um service worker vazio

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("main.js rodou!!!")
+if (serviceWorker in navigator){
+    navigator.serviceWorker.register("sw.js")
+    .then(function(r){
+        console.dir(r);
+    })
+    .catch(function(e){
+        console.dir(e);
+    })
+}
